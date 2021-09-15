c60ffae3e1bba6e5dc20a8aa1315388dd858aeba
bst0705 <xiangtaishiqiao0@gmail.com>
2021-09-15 04:45:30 +0000

    [Start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ce41f3f88b0a262e6862e9f06f56ea477f6d8ec383ad7ee97007917d62f4a7b54f2ea5cd316b1b3c27d13475c6c5964b66524143a1644af533aa9fc9a0c6c653'
